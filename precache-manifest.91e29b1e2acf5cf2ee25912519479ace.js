c45387d9df3ac0178cea2877720d5573d0dad7ae
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-08 08:44:24 +0000

    Deploy damianlewandowski/portfolio-v-3.0 to github.com/damianlewandowski/portfolio-v-3.0.git:gh-pages

---
+++ precache-manifest.91e29b1e2acf5cf2ee25912519479ace.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c150ebd757cffda22a8063abab7778d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "32893230f4259c1211a0",
+    "url": "/static/css/main.8ce4f2b6.chunk.css"
+  },
+  {
+    "revision": "8cb17b8a503a25091900",
+    "url": "/static/js/2.0d1d2ad0.chunk.js"
+  },
+  {
+    "revision": "c099810c3c102db7a44755001529c804",
+    "url": "/static/js/2.0d1d2ad0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32893230f4259c1211a0",
+    "url": "/static/js/main.5d6df049.chunk.js"
+  },
+  {
+    "revision": "359bd4c8387ae9fdd682",
+    "url": "/static/js/runtime-main.8c86f73e.js"
+  },
+  {
+    "revision": "d64dcc1f8d716a7d48296408c5ab86b3",
+    "url": "/static/media/dungeon-crawler.d64dcc1f.png"
+  },
+  {
+    "revision": "d5941e2e1efe3281f0ed5921e6dc2701",
+    "url": "/static/media/game-of-life.d5941e2e.png"
+  },
+  {
+    "revision": "c92aedc306ba05d5555492088df435cb",
+    "url": "/static/media/profile.c92aedc3.jpg"
+  },
+  {
+    "revision": "8cb9a4c1886d2d89e08b73d04b9c678f",
+    "url": "/static/media/voter.8cb9a4c1.png"
+  },
+  {
+    "revision": "1f0e2eb0e2a3cddf3b35c34d3460e166",
+    "url": "/static/media/weather-app.1f0e2eb0.png"
+  }
+]);
